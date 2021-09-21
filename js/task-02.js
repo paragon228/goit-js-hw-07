a2b50af297e2a3e5e52bdd3f8bb332b4d5ab284d
paragon228 <andre.golub2012@gmail.com>
2021-09-21 16:26:20 +0300

    2 дз

---
+++ js/task-02.js
@@ -0,0 +1,11 @@
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы']
+function createLi(array, id) {
+  const ingredientsEl = document.querySelector(id)
+  const ingredientEl = array.map(item => {
+    const itemEl = document.createElement('li')
+    itemEl.textContent = item
+    return itemEl
+  })
+  ingredientsEl.append(...ingredientEl)
+}
+createLi(ingredients, '#ingredients')
