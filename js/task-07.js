ab8b5bcf2401d003acf0a80e13b6bc61040237fd
paragon228 <andre.golub2012@gmail.com>
2021-09-22 14:37:53 +0300

    7дз

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const defSize = window
+  .getComputedStyle(document.querySelector('body'))
+  .getPropertyValue('font-size'); //находим font-size по default
+const vol = document.getElementById('font-size-control');
+vol.value = 0;
+const str = document.getElementById('text');
+vol.addEventListener('input', () => {
+  str.style.fontSize = `${parseInt(defSize) * (parseInt(vol.value) + 1)}px`;
+});
